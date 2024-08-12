379d33c49c9b523cfa362eeefc82c863edc4c01f
João Vitor Santiago <jv.santiago.souza@gmail.com>
2024-08-12 16:06:09 -0300

    v1.0

---
+++ poo.py
@@ -0,0 +1,12 @@
+class Caneca:
+    formato = "Cilíndrico"
+
+    def __init__(self, nome, marca, capacidade):
+        self.nome = nome
+        self.marca = marca
+        self.capacidade = capacidade
+
+caneca1 = Caneca("Top Caneca", "Stanley", "700mL")
+caneca2 = Caneca("Canequinha", "Pacco", "525mL")
+
+
